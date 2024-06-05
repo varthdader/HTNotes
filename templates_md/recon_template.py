ecf12d31d2fc7859a3b7c63e5e2ca7a3b5b91b9e
Varth Dader <varthdader@users.noreply.github.com>
2024-06-05 08:53:35 -0600

    Update recon_template.py
    Added Playbook Button and part of the playbook for scanning

--- templates_md/recon_template.py
+++ templates_md/recon_template.py
@@ -1,7 +1,14 @@
 
 def get_recon_template():
-    return '''Template
+    return '''## Target Info-sheet
 
+- DNS-Domain name:
+- Host name: INSERTHOSTNAME
+- OS:
+- Server:
+- Kernel:
+- Workgroup:
+- Windows domain:
 ---
 
 ## Nmap Summary
@@ -34,11 +41,72 @@ Ports UDP nmap format
 ```
 
 ---
+### Playbook Notes:
 
-## Enumeration
+Prep your attack Surface
+```
+msfdb run
+loadpath /TOOL/Metasploit-Code/modules
+workspace "HTB-Labs"
+```
+#### Recon Playbook
 
-## Port 80 - HTTP (Apache)
+##### Using Legion to Scan All the Things
+```
+legion
+    set workdir /Shared/PyCharms/HTB-Labs/Machines
+    set extensions txt,pdf,php,doc,docx,cgi,sh,pl,py,html,htm,phtml
+    set host INSERTIPADDRESS
+    set domain craft.htb
+    run
+```
 
+##### Syn-scan
+Always start with a stealthy scan to avoid closing ports.
+```
+db_nmap -sS INSERTIPADDRESS
+```
+##### Scan all ports, might take a while.
+```
+db_nmap INSERTIPADDRESS -p-
+```
+##### Service-version, default scripts, OS:
+```
+db_nmap INSERTIPADDRESS -sV -sC -O -p 111,222,333
+```
+
+##### Scan for UDP
+```
+db_nmap INSERTIPADDRESS -sU
+unicornscan -mU -v -I INSERTIPADDRESS
+```
+
+##### Connect to udp if one is open
+```
+nc -u INSERTIPADDRESS 48772
+```
+##### Monster scan
+```
+db_nmap INSERTIPADDRESS -p- -A -T4 -sC
+```
+##### Not-So-Sneaky Monster Scans
+```
+db_nmap -sV -sC -Pn -p 1-65535 -T5 --min-rate 1000 --max-retries 5 INSERTIPADDRESS
+db_nmap -sU -T4 -p1-1000 -sC -sV INSERTIPADDRESS
+masscan -e tun0 -p1-65535,U:1-65535 INSERTIPADDRESS --rate=100
+python /TOOL/nosey_neighbour/nosey.py INSERTIPADDRESS
+autorecon -o /home/kali/Documents/HTNotes/HTB/Machines/INSERTHOSTNAME/assets/ INSERTIPADDRESS
+masscan -e tun0 -p1-65535,U:1-65535 --max-rate=500 INSERTIPADDRESS 
+/TOOL/Target-Enumeration/sma.sh
+nmap -Pn -n --script vuln INSERTIPADDRESS
+```
+```button
+name Update Machine Playbook
+type link
+action obsidian://shell-commands/?vault=HTB&execute=28lv9wrf7u
+templater true
+color green
+```
 
 
 ---
